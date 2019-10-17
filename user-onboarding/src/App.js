5d7c1e3a7b8ee50dd505e17c333a47a96b79f2ae
Jackson McComas <jackson.mccomas@gmail.com>
2019-10-17 17:06:17 -0500

    completed MVP

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import FormikUserForm from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <FormikUserForm 
+      />
+    </div>
+  );
+}
+
+export default App;
